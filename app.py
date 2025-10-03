3b4eb77b94f7200d1573eba2e33003b46d9d8e52
Void <stas050595@gmail.com>
2025-10-03 18:23:26 +0000

    Optimization, better config.

--- app.py
+++ app.py
@@ -1,38 +1,67 @@
 import tkinter as tk
+import customtkinter as ctk
+import json , pathlib
 from tkinter import filedialog
 
 # window setup
-window = tk.Tk()
-window.geometry("600x450")
+window = ctk.CTk()
+window.geometry("610x450")
 window.resizable(width=False , height=False)
 window.title("VoidPad")
 
 # Variables
+
+app_path = pathlib.Path(__file__).resolve().parent
+
 file_selected = None
-path_to_file = tk.StringVar()
+path_to_file = ctk.StringVar()
 text_inputted = None
 
+default_config = {
+    "current_theme": "darkblue",
+    "themes": {
+        "darkblue": {
+            "bg_window": "#212e47",
+            "bg_button": "#212e60",
+            "hover_button": "#212e70",
+            "text_color": "#41608a"
+        },
+        "dark": {
+            "bg_window": "#394252",
+            "bg_button": "#272d38",
+            "hover_button": "#303745",
+            "text_color": "#48546e",
+        }
+    }
+}
 
-theme_config = []
-with open("theme.conf" , "r") as file:
-    for line in file.readlines():
-        theme_config.append(line.split()[0])
-    print("Done.")    
+def redo_config():
+    with open(f"{app_path}/theme.json" , "w") as file:
+        json.dump(default_config , file)
 
-theme = theme_config[0]
-bg_window = theme_config[1]
-bg_button = theme_config[2]
-activebg_button = theme_config[3]
-fg_button = theme_config[4]
-activefg_button = theme_config[5]
+def load_config():
+    try:
+        with open(f"{app_path}/theme.json" , "r") as file:
+            theme_config = json.load(file)
+        return theme_config
+    except (FileNotFoundError , json.JSONDecodeError):
+        redo_config()
+        return default_config 
 
-window.config(bg=bg_window)
+config = load_config()
+current_theme = config["current_theme"]
+bg_window = config["themes"][current_theme]["bg_window"]
+bg_button = config["themes"][current_theme]["bg_button"]
+hover_button = config["themes"][current_theme]["hover_button"]
+text_color = config["themes"][current_theme]["text_color"]
+
+window.configure(fg_color=bg_window)
 
 text_area_fontsize = 12
 text_editor_frame_x = 53
 
 # frames
-text_editor_frame = tk.Frame(window, width=10 ,  height=5 , border = 0)
+text_editor_frame = ctk.CTkFrame(window, width=10 ,  height=5)
 text_editor_frame.place(x=0 , y=30)
 
 # commands
@@ -43,8 +72,8 @@ def fetch_path():
         with open(file_selected , "r") as file:
             file.read()
         fetch_info_from_file()
-    except FileNotFoundError:
-        print("File Not Found!")
+    except (FileNotFoundError , IsADirectoryError) as e:
+        print(str(e))
 
 def choose_file():
     global file_selected
@@ -60,9 +89,12 @@ def choose_file():
 
 def fetch_info_from_file():
     text_area.delete("1.0" , "end")
-    with open(getting_path.get() , "r") as file:
-        for line in file.readlines():
-            text_area.insert("end" , line)
+    try:
+        with open(getting_path.get() , "r") as file:
+            for line in file.readlines():
+                text_area.insert("end" , line)
+    except (IsADirectoryError , FileNotFoundError) as e:
+        text_area.insert("1.0" , str(e))
 
 def file_selection():
     global getting_path
@@ -94,29 +126,25 @@ def save():
         with open(save_path , "w") as file:
             file.write(text_area.get("1.0" , "end-1c"))
 
-def change_theme():
-    global theme 
-    if theme == "darkblue":
-        bg_window = "#394252"
-        bg_button = "#272d38"
-        activebg_button = "#303745"
-        fg_button = "#3b4654"
-        activefg_button = "#48546e"
-        theme = "dark"
-    else:
-        bg_window = "#212e47"
-        bg_button = "#212e60"
-        activebg_button = "#212e70"
-        fg_button = "#41608a"
-        activefg_button = "#4c70b5"
-        theme = "darkblue"
-    with open("theme.conf" , "w") as file:
-        file.write(f"{theme} # theme\n{bg_window} # bg_window\n{bg_button} # bg_button\n{activebg_button} # activebg_button\n{fg_button} # fg_button\n{activefg_button} # activefg_button")
+def change_theme(currentTheme):
+    global current_theme , buttons
+    current_theme_index = list(config["themes"].keys()).index(currentTheme) + 1
+    try:
+        theme = config["themes"][list(config["themes"].keys())[current_theme_index]]
+    except IndexError:
+        current_theme_index = 0
+        theme = config["themes"][list(config["themes"].keys())[current_theme_index]]
+    
+    current_theme = list(config["themes"].keys())[current_theme_index]
+    
+    with open(f"{app_path}/theme.json" , "w") as file:
+        config["current_theme"] = current_theme
+        json.dump(config , file)
+
     for button in buttons:
-        button.config(bg=bg_button , activebackground=activebg_button , fg=fg_button , activeforeground=activefg_button)
-    window.config(bg=bg_window)
-    text_area.config(bg=bg_window)
-    file_name.config(bg=bg_window)
+        button.button.configure(fg_color=theme["bg_button"] , text_color=theme["text_color"] , hover_color=theme["hover_button"])
+    window.configure(fg_color=theme["bg_window"])
+    text_area.configure(bg=theme["bg_window"])
 
 def font_change(event=None , action=None):
     global text_area_fontsize
@@ -138,54 +166,62 @@ def slider(event=None , side=None):
         if text_editor_frame_x < 0:
             text_editor_frame_x += 25
 
-    text_editor_frame.place(x=text_editor_frame_x , y=80)
+    text_editor_frame.place(x=text_editor_frame_x , y=30)
 
 def refresh():
     # logic
     if file_selected is None and len(text_area.get("1.0" , "end-1c")) == 0:
-        file_name.config(text="No File Selected.")
+        file_name.configure(text="No File Selected.")
     elif len(text_area.get("1.0" , "end-1c")) > 0 and file_selected is None:
-        file_name.config(text="New file.")
-    if file_selected != None:
+        file_name.configure(text="New file.")
+    else:
         if len(file_selected) > 70:
             overflow = len(file_selected) - 70
-            file_name.config(text=f"{file_selected[:-overflow]}...")
+            file_name.configure(text=f"{file_selected[:-overflow]}...")
         else:
-            file_name.config(text=file_selected)
+            file_name.configure(text=file_selected)
 
     window.after(1 , refresh)
 
+def undo(text_area):
+    try:
+        text_area.edit_undo()
+    except tk.TclError:
+        pass
+
+def redo(text_area):
+    try:
+        text_area.edit_redo()
+    except tk.TclError:
+        pass
+
+class Button:
+    def __init__(self , text , font=("arial" , 16 , "bold") , size=(75 , 60) , pos=(0 , 0) , fg_color=bg_button , hover_color=hover_button , text_color=text_color , corner_radius=20 , command=lambda: None):
+        self.button = ctk.CTkButton(window , text=text , font=font , width=size[0] , height=size[1] , corner_radius=corner_radius , fg_color=fg_color , hover_color=hover_color , text_color=text_color , command=lambda: command())
+        self.button.place(x=pos[0] , y=pos[1])
 # GUI
 
-open_file = tk.Button(text="Open File" , font=("Arial" , 10 , "bold") , width=10 , height=3 , bg=bg_button , activebackground=activebg_button , fg=fg_button , activeforeground=activefg_button, highlightthickness=0 , border=0 , command=lambda: file_selection())
-open_file.place(x=500 , y=385)
+open_file = Button(text="Open File" , pos=(495 , 375) , command=lambda: file_selection())
 
-file_name = tk.Label(text=file_selected , font=("arial" , 10 , "bold") , fg="white" , bg=bg_window)
+file_name = ctk.CTkLabel(window , text=file_selected , font=("arial" , 20 , "bold") , text_color="white")
 file_name.place(x=0 , y=0)
 
 text_area = tk.Text(text_editor_frame , width=53, wrap="word", undo=True, font=("arial", text_area_fontsize , "bold"), fg="white", bg=bg_window, borderwidth = 0 , highlightthickness=0 , insertbackground="white")
 text_area.pack(fill="both")
 
-save_button = tk.Button(text="Save" , font=("arial" , 10 , "bold") , width=10 , height=3 , bg=bg_button , activebackground=activebg_button , fg=fg_button , activeforeground=activefg_button , highlightthickness=0 , border=0 , command=lambda: save())
-save_button.place(x=500 , y=0)
+save_button = Button(text="Save" , size=(110 , 55) , pos=(500 , 0), command=lambda: save())
 
-undo_button = tk.Button(text="Undo" , font=("arial" , 10 , "bold") , width=10 , height=3 , bg=bg_button , activebackground=activebg_button , fg=fg_button , activeforeground=activefg_button , highlightthickness=0 , border=0 , command=lambda: text_area.edit_undo())
-undo_button.place(x=500 , y=60)
+undo_button = Button(text="Undo" , size=(110 , 55) , pos=(500 , 60) , command=lambda: undo(text_area))
 
-redo_button = tk.Button(text="Redo" , font=("arial" , 10 , "bold") , width=10 , height=3 , bg=bg_button , activebackground=activebg_button , fg=fg_button , activeforeground=activefg_button , highlightthickness=0 , border=0 , command=lambda: text_area.edit_redo())
-redo_button.place(x=500 , y=120)
+redo_button = Button(text="Redo" , size=(110 , 55) , pos=(500 , 120) , command=lambda: redo(text_area))
 
-new_button = tk.Button(text="New" , font=("arial" , 10 , "bold") , width=10 , height=3 , bg=bg_button ,activebackground=activebg_button , fg=fg_button , activeforeground=activefg_button , highlightthickness=0 , border=0 , command=lambda: text_area.delete("1.0" , "end"))
-new_button.place(x=500 , y=180)
+new_button = Button(text="New" , size=(110 , 55) , pos=(500 , 180), command=lambda: text_area.delete("1.0" , "end"))
 
-themes_button = tk.Button(text="Themes" , font=("arial" , 10 , "bold") , width=10 , height=4 , bg=bg_button , activebackground=activebg_button , fg=fg_button , activeforeground=activefg_button , highlightthickness=0 , border=0 , command=lambda: change_theme())
-themes_button.place(x=500 , y=240)
+themes_button = Button(text="Themes" , size=(110 , 55) , pos=(500 , 240) , command=lambda: change_theme(current_theme))
 
-sliderplus_button = tk.Button(text="->" , font=("arial" , 10 , "bold") , width=3 , height=3 , bg=bg_button , activebackground=activebg_button , fg=fg_button , activeforeground=activefg_button , highlightthickness=0 , border=0 , command=lambda: slider(side="right"))
-sliderplus_button.place(x=550 , y=320)
+sliderplus_button = Button(text=">" , size=(40 , 65) , pos=(555 , 300) , command=lambda: slider(side="right"))
 
-sliderminus_button = tk.Button(text="<-" , font=("arial" , 10 , "bold") , width=3 , height=3 , bg=bg_button , activebackground=activebg_button , fg=fg_button , activeforeground=activefg_button , highlightthickness=0 , border=0 , command=lambda: slider(side="left"))
-sliderminus_button.place(x=500 , y=320)
+sliderminus_button = Button(text="<" , size=(40 , 65), pos=(500 , 300) , command=lambda: slider(side="left"))
 
 # shortcuts
 text_area.bind("<Control-plus>", lambda e: font_change(e, action="plus"))
@@ -195,6 +231,7 @@ text_area.bind("<Control-Right>", lambda e: slider(e , side="right"))
 text_area.bind("<Control-Left>" , lambda e: slider(e , side="left"))
 
 # button list
+
 buttons = [
     open_file,
     save_button,
@@ -205,6 +242,8 @@ buttons = [
     sliderplus_button,
     sliderminus_button
 ]
+
+window.update()
 
 refresh()
 
